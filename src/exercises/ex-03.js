084b208ef8f704245346a8f01d2b407f0c6c5e7b
Nando Stöckli <e228448@edu.sbl.ch>
2026-01-16 12:44:07 +0000

    kommentar

--- src/exercises/ex-03.js
+++ src/exercises/ex-03.js
@@ -1,6 +1,8 @@
 const myname = process.argv[2];
+// Die Funktion Number() wandelt den String in eine Zahl um //
 const myage = Number(process.argv[3]);
 const maturAge = myage + 4;
 
+// Ausgabe der Begrüssung und des Alters bei der Matur//
 console.log(`Hallo ${myname}, mein Alter ist ${myage}!`);
 console.log(`Bei der Matur in 4 Jahren werde ich ${maturAge} Jahre alt sein!`);
