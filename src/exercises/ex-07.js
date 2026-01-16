26bb64f57804b7cc651963c0945d1917982455e5
Nando Stöckli <e228448@edu.sbl.ch>
2026-01-16 13:48:54 +0000

    alle zahlen dividieren

--- src/exercises/ex-07.js
+++ src/exercises/ex-07.js
@@ -1,10 +1,10 @@
 const mynumber = Number(process.argv[2]);
-const remainder = mynumber % 3;
-
+const remainder = mynumber % Number(process.argv[3]);
+const divider = Number(process.argv[3]);
 console.log(`Deine Zahl ist ${mynumber}`);
 
 if (remainder === 0) {
-	console.log(`${mynumber} ist durch 3 teilbar.`);
+	console.log(`${mynumber} ist durch ${divider} teilbar.`);
 } else {
-	console.log(`${mynumber} ist nicht durch 3 teilbar.`);
+	console.log(`${mynumber} ist nicht durch ${divider} teilbar.`);
 }
