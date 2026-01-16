003149d546f909a74cc269e4cd1b631f5ae9a52a
Nando Stöckli <e228448@edu.sbl.ch>
2026-01-16 12:43:55 +0000

    myage + 4 wegen Matur...

---
+++ src/exercises/ex-03.js
@@ -0,0 +1,6 @@
+const myname = process.argv[2];
+const myage = Number(process.argv[3]);
+const maturAge = myage + 4;
+
+console.log(`Hallo ${myname}, mein Alter ist ${myage}!`);
+console.log(`Bei der Matur in 4 Jahren werde ich ${maturAge} Jahre alt sein!`);
