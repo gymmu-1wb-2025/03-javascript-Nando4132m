6a04ed5f9448aba0f2ae4c6ed1e3bb8900543752
Nando Stöckli <e228448@edu.sbl.ch>
2026-01-16 13:42:52 +0000

    aus aufgabe 06

---
+++ src/exercises/ex-07.js
@@ -0,0 +1,10 @@
+const mynumber = Number(process.argv[2]);
+const remainder = mynumber % 3;
+
+console.log(`Deine Zahl ist ${mynumber}`);
+
+if (remainder === 0) {
+	console.log(`${mynumber} ist durch 3 teilbar.`);
+} else {
+	console.log(`${mynumber} ist nicht durch 3 teilbar.`);
+}
